cf877561fca1de49b12cfb67b8cd14875b671106
Alan Glenn <alanglenn74@gmail.com>
2025-11-01 13:25:09 -0500

    feat: show material ui works

--- app/look/page.tsx
+++ app/look/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { useFoodGet } from "@/hooks/api"
+import { Box, styled } from "@mui/material";
+
+const LookIn = styled(Box)({
+    color: 'green',
+})
 
 export default function Look() {
     const response= useFoodGet({id:2705385 })
-    return <>Look!: {JSON.stringify(response.data?.description || {}, null, 2)}</>
+    return <LookIn>{`Look!: ${{JSON.stringify(response.data?.description || {}, null, 2)}}`}</LookIn>
 }
