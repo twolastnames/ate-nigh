f24892f464132c0d516ee92906cf516a0591d81f
Alan Glenn <alanglenn74@gmail.com>
2026-03-01 12:00:31 -0600

    refactor: update Eaten component to display items in ounces and improve table layout

--- components/feature/eaten/Eaten.tsx
+++ components/feature/eaten/Eaten.tsx
@@ -1,34 +1,24 @@
 "use client";
 import React from "react";
-import { Box, styled } from "@mui/material";
-import { EatenTypes } from "./EatenTypes";
-
-const Container = styled(Box)({
-})
-
-export function Eaten(props: EatenTypes) {
-    return <Container data-testid="Eaten"></Container>
-}
-"use client";
-import React from "react";
 import { Container } from "@mui/material";
 import { EatenTypes } from "./EatenTypes";
 import { Table } from "../../ui/table/Table";
+import { grams } from "@/util/units";
 
 export function Eaten(props: EatenTypes) {
-  const headers = ["Time", "Food ID", "Amount (g)"];
-  const data = [...props.ates]
+  const headers = ["Time", "Food ID", "Amount (oz)"];
+  const data = [...props.data]
     .sort((a, b) => new Date(b.time).getTime() - new Date(a.time).getTime())
     .map(ate => [
       new Date(ate.time).toLocaleString(),
       ate.foodId,
-      ate.amount
+      grams(ate.amount).asOunces(),
     ]);
 
   return (
     <Container data-testid="Eaten">
       <Table 
-        title="Eaten Items"
+        title="Items Eaten"
         headers={headers}
         data={data}
       />
